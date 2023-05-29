9323421b2a68bceee49ef4ebf23b71bef423c6ba
Andreas Visagie u21430901 <u21430901@tuks.co.za>
2023-05-29 15:47:31 +0200

    Mock GUI
    This is using a swing window builder in Intellij

---
+++ src/ClientGUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class ClientGUI {
+    private JPanel panel1;
+    private JTextField txtMailFrom;
+    private JTextField txtMailTo;
+    private JTextArea txtData;
+    private JButton btnSend;
+}
